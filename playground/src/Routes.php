5f7e8b44f6bcacbd133aed0688daf572ee255b65
wkarts <57051272+wkarts@users.noreply.github.com>
2026-02-06 01:29:00 -0300

    Remove redundant SDK API route mappings
    Removed unused API routes for SDK calls.

--- playground/src/Routes.php
+++ playground/src/Routes.php
@@ -13,8 +13,6 @@ use Playground\Controllers\SdkProxyController;
 use Playground\Controllers\WebhookController;
 use Playground\Utils\FileStore;
 use Slim\App;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
 final class Routes
 {
@@ -60,24 +58,5 @@ final class Routes
         $app->post('/raw/run', [$raw, 'run']);
 
         $app->get('/downloads/{name}', [new FileStore($bootstrap->basePath()), 'download']);
-
-
-        $app->map(['GET','POST','OPTIONS'], '/api/sdk/call/{service}/{method}', function (Request $request, Response $response, array $args) use ($controller) {
-            // Preflight CORS (se precisar)
-            if (strtoupper($request->getMethod()) === 'OPTIONS') {
-                return $response->withStatus(204);
-            }
-        
-            return $controller->handleCall($request, $response, $args);
-        });
-        
-        $app->map(['GET','POST','OPTIONS'], '/api/sdk/{service}/{method}', function (Request $request, Response $response, array $args) use ($controller) {
-            if (strtoupper($request->getMethod()) === 'OPTIONS') {
-                return $response->withStatus(204);
-            }
-        
-            return $controller->handleCall($request, $response, $args);
-        });  
-        
     }
 }
