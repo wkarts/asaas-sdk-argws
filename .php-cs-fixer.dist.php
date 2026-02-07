581eb3c83e0d8048ea823fdfcff5ca998bf0c3fb
wkarts <57051272+wkarts@users.noreply.github.com>
2026-02-06 21:48:04 -0300

    Fix syntax error in .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,7 +10,7 @@ return (new Config())
     ->setParallelConfig(ParallelConfigFactory::detect()) // @TODO 4.0 no need to call this manually
     ->setRiskyAllowed(false)
     ->setRules([
-        '@auto' => true
+        '@auto' => true,
     ])
     // 💡 by default, Fixer looks for `*.php` files excluding `./vendor/` - here, you can groom this config
     ->setFinder(
